e99d3a309b22153b4536af9b311b25fff8ab373a
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2025-06-22 08:35:00 -0300

    refact: ajuste nas validacoes impedindo enviar vazio ou null

--- src/main/java/com/lucas/validators/PessoasValidator.java
+++ src/main/java/com/lucas/validators/PessoasValidator.java
@@ -17,8 +17,17 @@ public class PessoasValidator {
         if (pessoasEntity.getId() != null && repository.existsById(pessoasEntity.getId())) {
             throw new IllegalArgumentException("Essa pessoa já existe.");
         }
+        if (pessoasEntity.getCpf() == null || pessoasEntity.getCpf().isEmpty()) {
+            throw new CpfException("CPF não pode ser nulo ou vazio.");
+        }
         if (repository.existsByCpf(pessoasEntity.getCpf())) {
             throw new CpfException("CPF já cadastrado.");
+        }
+        if(pessoasEntity.getTelefone() == null || pessoasEntity.getTelefone().isEmpty()) {
+            throw new CpfException("Telefone não pode ser nulo ou vazio.");
+        }
+        if(pessoasEntity.getEmail() == null || pessoasEntity.getEmail().isEmpty()) {
+            throw new EmailException("Email não pode ser nulo ou vazio.");
         }
         if (repository.existsByEmail(pessoasEntity.getEmail())) {
             throw new EmailException("Email já cadastrado.");
